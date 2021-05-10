79248916b5caedc052fd3c4476f68c9e00843feb
YORICHEONG <2351170798@qq.com>
2021-05-10 12:36:02 +0800

    push

---
+++ src/app/Test.java
@@ -0,0 +1,10 @@
+package app;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
