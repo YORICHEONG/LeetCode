4e9ad95b7aec544d16d5a5681cfd2c019746a082
YORICHEONG <2351170798@qq.com>
2021-05-12 13:00:06 +0800

    push

--- src/app/LeetCode/CommomElement.java
+++ src/app/LeetCode/CommomElement.java
@@ -32,6 +32,7 @@ public class CommomElement {
         int i = 0;
         int j = 0;
         List<Integer> list = new ArrayList<Integer>();
+        //分别用两个指标来对两个数组进行遍历比较，将相同的值添加到集合中
         while(i<nums1.length && j<nums2.length){
             if(nums1[i]<nums2[j]){
                 i++;
@@ -43,6 +44,7 @@ public class CommomElement {
                 j++;
             }
         }
+        //将集合转成数组
         int[] result = new int[list.size()];
         for(int k = 0; k<list.size(); k++){
             result[k] = list.get(k);
