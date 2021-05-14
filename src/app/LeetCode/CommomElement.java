cb72bad74c8f28be52125677e6c2e565a44e3f4d
YORICHEONG <2351170798@qq.com>
2021-05-14 08:46:09 +0800

    Device connect Test

--- src/app/LeetCode/CommomElement.java
+++ src/app/LeetCode/CommomElement.java
@@ -2,7 +2,6 @@ package app.LeetCode;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
