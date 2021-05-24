5dcb606e76bf9f913194cdf90309d34913c15196
YORICHEONG <2351170798@qq.com>
2021-05-25 01:52:21 +0800

    push

---
+++ src/app/LeetCode/Tree/MaxDepth.java
@@ -0,0 +1,12 @@
+package app.LeetCode.Tree;
+/**
+ *  给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ */
+public class MaxDepth {
+  public int solution(TreeNode root){
+    //使用递归的方式
+    return root==null?0:Integer.max(solution(root.left), solution(root.right))+1;
+  }
+}
