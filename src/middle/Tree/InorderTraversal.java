c2d048fc0066843d03aefecb2d30416345c7a8f0
YORICHEONG <2351170798@qq.com>
2021-05-31 00:44:35 +0800

    commit

---
+++ src/middle/Tree/InorderTraversal.java
@@ -0,0 +1,20 @@
+package middle.Tree;
+import java.util.*;
+
+public class InorderTraversal {
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> result = new ArrayList<Integer>();
+    
+    mid(root,result);
+    return result;
+  }
+
+  public void mid(TreeNode root,List<Integer> list){
+    if(root==null){
+      return;
+    }
+    mid(root.left,list);
+    list.add(root.val);
+    mid(root.right,list);
+  }
+}
