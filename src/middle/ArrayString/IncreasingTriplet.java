0a139eed356a581c3d92a9f1ff06463b1ac9a7e0
YORICHEONG <2351170798@qq.com>
2021-05-30 11:30:52 +0800

    commit

---
+++ src/middle/ArrayString/IncreasingTriplet.java
@@ -0,0 +1,23 @@
+package middle.ArrayString;
+
+public class IncreasingTriplet {
+  public boolean increasingTriplet(int[] nums) {
+    int length = nums.length;
+    if(length<3){
+      return false;
+    }
+    int small = Integer.MAX_VALUE;
+    int mid = Integer.MAX_VALUE;
+
+    for(int value:nums){
+      if(value<=small){
+        small = value;
+      }else if(value<=mid){
+        mid = value;
+      }else if(value>mid){
+        return true;
+      }
+    }
+    return false;
+  }
+}
