3fe8c4c2db1a9090cc2e0cc8a52662d1d9d92a98
YORICHEONG <2351170798@qq.com>
2021-06-24 15:47:17 +0800

    commit

---
+++ src/DailyPunch/maxPoints-20210624.java
@@ -0,0 +1,10 @@
+/*
+	2021-06-24 15:41:50
+	YORICHEONG
+	Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+*/
+class Solution {
+    public int maxPoints(int[][] points) {
+        
+    }
+}
---
+++ src/higher/StringArray/ProductExceptSelf.java
@@ -0,0 +1,20 @@
+/*
+	2021-06-24 15:19:27
+	YORICHEONG
+	Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+	The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+	You must write an algorithm that runs in O(n) time and without using the division operation
+
+*/
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        
+    }
+
+    public int[] productExceptSelf01(int[] nums) {
+    	int length = nums.length;
+    	int[] answer = new int[length];
+    	
+    	
+    }
+}
