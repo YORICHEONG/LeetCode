21da6d4eeff7505b2aff199440f92ed2930a004d
YORICHEONG <2351170798@qq.com>
2021-06-25 09:26:00 +0800

    commit

--- src/DailyPunch/maxPoints_20210624.java
+++ src/DailyPunch/maxPoints_20210624.java
@@ -1,8 +1,9 @@
+package DailyPunch;
 /*
-	2021-06-24 15:41:50
-	YORICHEONG
-	Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
-*/
+ *  2021-06-24 15:41:50
+ *	YORICHEONG
+ *	Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+ */
 import java.util.*;
 class Solution {
     public int maxPoints(int[][] points) {
