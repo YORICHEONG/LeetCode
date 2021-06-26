738ad985c33efabf193088f51b027c2e1a49e548
YORICHEONG <2351170798@qq.com>
2021-06-26 16:25:48 +0800

    commit

---
+++ src/DailyPunch/slidingPuzzle_20210626.java
@@ -0,0 +1,12 @@
+package DailyPunch;
+import java.util.*;
+
+class Solution{
+    int[][] neighbors = {{1, 3}, {0, 2, 4}, {1, 5}, {0, 4}, {1, 3, 5}, {2, 4}};
+
+    public int slidingPuzzle(int[][] board) {
+        StringBuilder 
+
+        return null;
+    }
+}
