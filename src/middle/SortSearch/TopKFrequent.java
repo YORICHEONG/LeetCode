e4aa537eb1507834a8042cadc58e3f22b2a96bdd
YORICHEONG <2351170798@qq.com>
2021-06-26 22:09:25 +0800

    commit

--- src/middle/SortSearch/TopKFrequent.java
+++ src/middle/SortSearch/TopKFrequent.java
@@ -1,38 +1,37 @@
 package middle.SortSearch;
+
 import java.rmi.server.RemoteRef;
 import java.util.*;
 
-import javafx.beans.property.MapProperty;
-
-public class TopKFrequent{
+public class TopKFrequent {
 	public int[] topKFrequent(int[] nums, int k) {
-		HashMap<Integer,Integer> hashMap = new HashMap();
-		for(int num:nums){
-			if(hashMap.containsKey(num)){
-				hashMap.put(num, hashMap.get(num)+1);
+		HashMap<Integer, Integer> hashMap = new HashMap();
+		for (int num : nums) {
+			if (hashMap.containsKey(num)) {
+				hashMap.put(num, hashMap.get(num) + 1);
 			}
-			hashMap.put(num,1);
+			hashMap.put(num, 1);
 		}
-		
-		PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(new Comparator<int[]>(){
-			
+
+		PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(new Comparator<int[]>() {
+
 			@Override
-			public int compare(int[] a,int[] b){
-				return a[1]-b[1];
+			public int compare(int[] a, int[] b) {
+				return a[1] - b[1];
 			}
 		});
 
-		for(Map.Entry<Integer,Integer> entry:hashMap.entrySet()){
+		for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
 			int num = entry.getKey();
 			int value = entry.getValue();
-			if(priorityQueue.size()==k){
-				//从堆中取出一个数据来进行比较大小
-				if(priorityQueue.peek()[1]<value){
+			if (priorityQueue.size() == k) {
+				// 从堆中取出一个数据来进行比较大小
+				if (priorityQueue.peek()[1] < value) {
 					priorityQueue.poll();
-					priorityQueue.offer(new int[]{num,value});
+					priorityQueue.offer(new int[] { num, value });
 				}
-			}else{
-				priorityQueue.offer(new int[]{num,value});
+			} else {
+				priorityQueue.offer(new int[] { num, value });
 			}
 		}
 
@@ -41,5 +40,5 @@ public class TopKFrequent{
 			result[i] = priorityQueue.poll()[0];
 		}
 		return result;
-    }
+	}
 }
