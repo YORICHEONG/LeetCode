143aad86ce44b0ab49c156def18815216897244f
YORICHEONG <2351170798@qq.com>
2021-07-01 16:34:09 +0800

    commit

---
+++ src/higher/stringarray/MinWindow.java
@@ -0,0 +1,32 @@
+package higher.stringarray;/**
+ * @Description
+ * @author YORICHEONG
+ * @create 2021-07-01 15:23
+ */
+
+/**
+ * @description: 给你一个字符串 s 、一个字符串 t 。返回 s 中涵盖 t 所有字符的最小子串。如果 s 中不存在涵盖 t 所有字符的子串，则返回空字符串 "" 。
+ * 注意：如果 s 中存在这样的子串，我们保证它是唯一的答案
+ * 输入：s = "ADOBECODEBANC", t = "ABC"
+ * 输出："BANC"
+ *
+ * 输入：s = "a", t = "a"
+ * 输出："a"
+ * @author YORICHEONG
+ * @date 2021/7/1 15:23
+ * @version 1.0
+ */
+public class MinWindow {
+    /**
+     *
+     * @description:
+     * @param: [java.lang.String, java.lang.String] [s, t] 
+     * @return: java.lang.String
+     * @author YORICHEONG
+     * @date: 2021/7/1 15:295
+     */
+    public String minWindow(String s, String t) {
+        // 如何确定最小的长度的子串
+        return null;
+    }
+}
