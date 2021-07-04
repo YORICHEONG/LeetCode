3be00b7f9bdc0e071babe6d8edf199853138f72d
YORICHEONG <2351170798@qq.com>
2021-07-04 14:02:41 +0800

    commit

--- src/english/top100/FindMedianSortedArrays.java
+++ src/english/top100/FindMedianSortedArrays.java
@@ -1,25 +1,24 @@
-package english.top100;/**
- * @Description
- * @author YORICHEONG
- * @create 2021-07-02 18:29
- */
+package english.top100;
+
+/**
+* @Description
+* @author YORICHEONG
+* @create 2021-07-02 18:29
+*/
 
 import java.util.Arrays;
 
 /**
- * @description: Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
- * The overall run time complexity should be O(log (m+n)).
+ * @description: Given two sorted arrays nums1 and nums2 of size m and n
+ *               respectively, return the median of the two sorted arrays. The
+ *               overall run time complexity should be O(log (m+n)).
  *
- * Input: nums1 = [1,3], nums2 = [2]
- * Output: 2.00000
- * Explanation: merged array = [1,2,3] and median is 2.
+ *               Input: nums1 = [1,3], nums2 = [2] Output: 2.00000 Explanation:
+ *               merged array = [1,2,3] and median is 2.
  *
- * Input: nums1 = [], nums2 = [1]
- * Output: 1.00000
+ *               Input: nums1 = [], nums2 = [1] Output: 1.00000
  *
- * Example 5:
- * Input: nums1 = [2], nums2 = []
- * Output: 2.00000
+ *               Example 5: Input: nums1 = [2], nums2 = [] Output: 2.00000
  *
  *
  * @author YORICHEONG
@@ -28,21 +27,21 @@ import java.util.Arrays;
  */
 public class FindMedianSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.length == 0 && nums2.length == 1) {
+        if (nums1.length == 0 && nums2.length == 1) {
             return nums2[0];
         }
-        if(nums2.length == 0 && nums1.length == 1) {
+        if (nums2.length == 0 && nums1.length == 1) {
             return nums1[0];
         }
-        if(nums1.length == 0 && nums2.length == 0) {
+        if (nums1.length == 0 && nums2.length == 0) {
             return 0;
         }
 
         int m = nums1.length;
         int n = nums2.length;
-        boolean isEven = (m+n)%2==0;
+        boolean isEven = (m + n) % 2 == 0;
 
-        if(m > n) {
+        if (m > n) {
             return findMedian(nums1, nums2);
         } else {
             return findMedian(nums2, nums1);
@@ -61,9 +60,12 @@ public class FindMedianSortedArrays {
 
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
-            int j = (l+s)/2 - i;
+            int j = (l + s) / 2 - i;
 
-            if(i < l && longArray[j-1] > shortArray[i])
+            if (i < l && longArray[j - 1] > shortArray[i]) {
+
+            }
         }
+        return 0;
     }
 }
