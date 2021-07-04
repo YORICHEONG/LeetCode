afd1c8dff4a8f3f3f5adb1034f0fc82f013f29df
YORICHEONG <2351170798@qq.com>
2021-07-04 14:01:55 +0800

    20210704

---
+++ src/dailypunch/frequencySort_20210703.java
@@ -0,0 +1,49 @@
+package dailypunch;
+
+import java.util.*;
+
+/**
+ * @author YORICHEONG
+ * @date 09点21分
+ * @description 给定一个字符串，请将字符串里的字符按照出现的频率降序排列。
+ * 示例 1:
+ *
+ * 输入:
+ * "tree"
+ *
+ * 输出:
+ * "eert"
+ *
+ * 解释:
+ * 'e'出现两次，'r'和't'都只出现一次。
+ * 因此'e'必须出现在'r'和't'之前。此外，"eetr"也是一个有效的答案
+
+ */
+public class frequencySort_20210703 {
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int length = s.length();
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            int frequency = map.getOrDefault(c, 0) + 1;
+            map.put(c, frequency);
+        }
+        List<Character> list = new ArrayList<Character>(map.keySet());
+        Collections.sort(list, (a, b) -> map.get(b) - map.get(a));
+        StringBuffer sb = new StringBuffer();
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            char c = list.get(i);
+            int frequency = map.get(c);
+            for (int j = 0; j < frequency; j++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
