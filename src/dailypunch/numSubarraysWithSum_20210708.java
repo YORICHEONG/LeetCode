38f66d2e7dd0b6b7a253142e0dff25a472dd7a3b
pengzhongjie <pengzhongjie@meizu.com>
2021-07-08 10:47:55 +0800

    commit

--- src/dailypunch/numSubarraysWithSum_20210708.java
+++ src/dailypunch/numSubarraysWithSum_20210708.java
@@ -6,13 +6,35 @@ package dailypunch;
  */
 public class numSubarraysWithSum_20210708 {
     /**
-     * 
+     * 使用滑动窗口来遍历查找符合的子数组
      * @param nums
      * @param goal
      * @return
      */
     public int numSubarraysWithSum(int[] nums, int goal) {
+        int numsLength = nums.length;
+        int windowLeft1 = 0;
+        int windowLeft2 = 0;
+        int windowRight = 0;
+        int windowSum1 = 0;
+        int windowSum2 = 0;
         
-        return -1;
+        int resultCount = 0;
+        while(windowRight < numsLength) {
+            windowSum1 += nums[windowRight];
+            while( windowLeft1 <= windowRight && windowSum1 > goal) {
+                windowSum1 -= nums[windowLeft1];
+                windowLeft1++;
+            }
+            windowSum2 += nums[windowRight];
+            while(windowLeft2 <= windowRight && windowSum2 >= goal) {
+                windowSum2 -= nums[windowLeft2];
+                windowLeft2++;
+            }
+
+            resultCount += windowLeft2-windowLeft1;
+            windowRight++;
+        }
+        return resultCount;
     }
 }
