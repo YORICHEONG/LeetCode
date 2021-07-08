1f1053bc6eb5812e9457dd5603d1cc514964c9b6
pengzhongjie <pengzhongjie@meizu.com>
2021-07-08 10:18:44 +0800

    commit

---
+++ src/dailypunch/numSubarraysWithSum_20210708.java
@@ -0,0 +1,18 @@
+package dailypunch;
+/**
+ * @author YORICHEONG
+ * @data 2021-07-08 10:18:05
+ * @descrption 
+ */
+public class numSubarraysWithSum_20210708 {
+    /**
+     * 
+     * @param nums
+     * @param goal
+     * @return
+     */
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        
+        return -1;
+    }
+}
