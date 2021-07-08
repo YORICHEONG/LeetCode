daec8734db6e6359a349d81c015b6eec97ba105b
pengzhongjie <pengzhongjie@meizu.com>
2021-07-08 15:24:30 +0800

    commit

---
+++ src/higher/treegraph/MaxPathSum.java
@@ -0,0 +1,14 @@
+package higher.treegraph;
+
+/**
+ * @author YORICHEONG
+ * @date 2021-07-08 14:16:34
+ * @description 路径 被定义为一条从树中任意节点出发，沿父节点-子节点连接，达到任意节点的序列。
+ * 同一个节点在一条路径序列中 至多出现一次 。该路径 至少包含    一个 节点，且不一定经过根节点。
+ */
+public class MaxPathSum {
+    public int maxPathSum(TreeNode root) {
+        
+        return -1;
+    }
+}
