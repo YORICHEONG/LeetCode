31a4d428a8bb51cdc9ce5c5b9fafcc0b07b8e7fa
pengzhongjie <pengzhongjie@meizu.com>
2021-08-25 15:52:52 +0800

    20210825

--- src/top100/GenerateParenthesis.java
+++ src/top100/GenerateParenthesis.java
@@ -7,11 +7,14 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @description:
@@ -28,18 +31,53 @@ public class GenerateParenthesis {
      * @date : 2021/8/19
      */
     public List<String> generateParenthesis(int n) {
-
         return null;
     }
 
     public static void main(String[] args) {
-        TimeUnit unit;
-        BlockingQueue workQueue;
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(20, 50, 60, TimeUnit.MICROSECONDS, new LinkedBlockingDeque<Runnable>());
-        for (int i = 0; i < 1000000; i++) {
-            
+        List<String> list = new ArrayList<>();
+        list.add("23");
+        list.add("234");
+        list.add("234253");
+        list.add("78");
+        list.add("56");
+        Map<String, Double> map = list.stream().collect(Collectors.toMap(Function.identity(), s -> (double)(int)System.currentTimeMillis()));
+        List<Map<String, Double>> result = splitMap(map, 1);
+        for (Map<String, Double> m: result) {
+            System.out.println(m.toString());
         }
+//        List<Map<String, Double>> result = splitAppIdArray(,3, (int)System.currentTimeMillis() / 1000);
+//        for (Map<String, Double> map: result) {
+//            System.out.println(map.size());
+//            System.out.println(map.toString());
+//            System.out.println("==================");
+//        }
     }
+
+
+    public static List<Map<String, Double>> splitMap(Map<String, Double> chunkMap, int chunkNum) {
+        if (chunkMap == null || chunkMap.size() == 0 || chunkNum <= 0) {
+            return null;
+        }
+        int i = 1;
+        List<Map<String, Double>> total = new ArrayList<>();
+        Map<String, Double> tem = new HashMap<>();
+        for(Map.Entry<String, Double> entry : chunkMap.entrySet()) {
+            tem.put(entry.getKey(), entry.getValue());
+            if(i == chunkNum) {
+                total.add(tem);
+                tem = new HashMap<>();
+                i = 0;
+            }
+            i++;
+        }
+        if (!(tem == null || tem.isEmpty())) {
+            total.add(tem);
+        }
+
+        return total;
+    }
+
 }
 
 class Task implements Runnable {
