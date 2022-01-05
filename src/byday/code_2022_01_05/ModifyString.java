a8cf251b230b8a684981098b951be1ef4a36dfc3
pengzhongjie <pengzhongjie@meizu.com>
2022-01-05 19:29:42 +0800

    Commit by 2022-01-05-02

---
+++ src/byday/code_2022_01_05/ModifyString.java
@@ -0,0 +1,28 @@
+package byday.code_2022_01_05;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+/**
+ * @Description
+ * @Author YORICHEONG yoricheong@gmail.com precent99@qq.com
+ * @Date 2022/1/5 19:14
+ * @Tags
+ **/
+public class ModifyString {
+    public String modifyString(String s) {
+        StringBuilder sb = new StringBuilder().append(s);
+        int l,r = 0;
+        for(int i = sb.length() - 1; i >= 0; i--) {
+            if(sb.charAt(i) == '?') {
+                continue;
+            } else {
+
+            }
+        }
+        return null;
+    }
+
+    private String generate(int lenght, int i) {
+
+    }
+}
